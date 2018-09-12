8b12eeb16c687bc40847d6c72e041dac195b7faa
Fred Zhang <zhangxu.fred@gmail.com>
2018-09-12 19:47:51 +0800

    three problems

---
+++ age_in_days.py
@@ -0,0 +1,56 @@
+# By Websten from forums
+#
+# Given your birthday and the current date, calculate your age in days.
+# Account for leap days.
+#
+# Assume that the birthday and current date are correct dates (and no
+# time travel).
+#
+
+daysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+def isLeapYear(year):
+    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+
+
+def calculate_days(year, month, day):
+    y = 0
+    m = 0
+    r = 0
+    while y < year:
+        if isLeapYear(y):
+            r = r + 366
+        else:
+            r = r + 365
+        y = y + 1
+    while m < month:
+        if isLeapYear(y) and m == 2:
+            r = r + 29
+        else:
+            r = r + daysOfMonths[m - 1]
+        m = m + 1
+    r = r + day
+    return r
+
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    return calculate_days(year2, month2, day2) - calculate_days(year1, month1, day1)
+
+# Test routine
+
+def test():
+    test_cases = [((2012, 1, 1, 2012, 2, 28), 58),
+                  ((2012, 1, 1, 2012, 3, 1), 60),
+                  ((2011, 6, 30, 2012, 6, 30), 366),
+                  ((2011, 1, 1, 2012, 8, 8), 585),
+                  ((1900, 1, 1, 1999, 12, 31), 36523)]
+    for (args, answer) in test_cases:
+        result = daysBetweenDates(*args)
+        if result != answer:
+            print "Test with data:", args, "failed"
+        else:
+            print "Test case passed!"
+
+
+test()
--- test.py
+++ test.py
@@ -1,11 +1,4 @@
 # encoding: utf-8
 
-while n != 1:
-    if n % 2 == 0:  # n 是偶数
-        n = n / 2
-        if n == 1:
-            print 'stop'
-    else:
-        n = 3 * n + 1
-        if n == 1:
-            print 'stop'
+a = -121
+print (a)
