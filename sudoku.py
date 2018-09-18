d4170d6e57699e271a86cdb5ff1a98a153d5bfc8
Fred Zhang <zhangxu.fred@gmail.com>
2018-09-18 14:35:24 +0800

    add another 3 tasks

---
+++ sudoku.py
@@ -0,0 +1,113 @@
+# THREE GOLD STARS
+
+# Sudoku [http://en.wikipedia.org/wiki/Sudoku]
+# is a logic puzzle where a game
+# is defined by a partially filled
+# 9 x 9 square of digits where each square
+# contains one of the digits 1,2,3,4,5,6,7,8,9.
+# For this question we will generalize
+# and simplify the game.
+
+# Define a procedure, check_sudoku,
+# that takes as input a square list
+# of lists representing an n x n
+# sudoku puzzle solution and returns the boolean
+# True if the input is a valid
+# sudoku square and returns the boolean False
+# otherwise.
+
+# A valid sudoku square satisfies these
+# two properties:
+
+#   1. Each column of the square contains
+#       each of the whole numbers from 1 to n exactly once.
+
+#   2. Each row of the square contains each
+#       of the whole numbers from 1 to n exactly once.
+
+# You may assume the the input is square and contains at
+# least one row and column.
+
+correct = [[1, 2, 3],
+           [2, 3, 1],
+           [3, 1, 2]]
+
+incorrect = [[1, 2, 3, 4],
+             [2, 3, 1, 3],
+             [3, 1, 2, 3],
+             [4, 4, 4, 4]]
+
+incorrect2 = [[1, 2, 3, 4],
+              [2, 3, 1, 4],
+              [4, 1, 2, 3],
+              [3, 4, 1, 2]]
+
+incorrect3 = [[1, 2, 3, 4, 5],
+              [2, 3, 1, 5, 6],
+              [4, 5, 2, 1, 3],
+              [3, 4, 5, 2, 1],
+              [5, 6, 4, 3, 2]]
+
+incorrect4 = [['a', 'b', 'c'],
+              ['b', 'c', 'a'],
+              ['c', 'a', 'b']]
+
+incorrect5 = [[1, 1.5],
+              [1.5, 1]]
+
+
+def rotate_square(square):  # 把数独逆时针旋转90度
+    new_square = []
+    j = 0
+    row_length = len(square)
+    while j < row_length:
+        new_row = []
+        for row in square:
+            new_row.append(row[j])
+        new_square.append(new_row)
+        j = j + 1
+    return new_square
+
+
+def check_sudoku(square):
+    test_row = []
+    i = 1
+    row_length = len(square)
+    while i < row_length + 1:
+        test_row.append(i)
+        i = i + 1
+
+    new_square = rotate_square(square)
+
+    for row in square:
+        row = sorted(row)
+        if row != test_row:
+            return False
+
+    for row in new_square:
+        row = sorted(row)
+        if row != test_row:
+            return False
+
+    return True
+
+
+print check_sudoku(incorrect)
+# >>> False
+
+print check_sudoku(correct)
+# >>> True
+
+print check_sudoku(incorrect2)
+# >>> False
+
+print check_sudoku(incorrect3)
+# >>> False
+
+print check_sudoku(incorrect4)
+# >>> False
+
+print check_sudoku(incorrect5)
+# >>> False
+
+
