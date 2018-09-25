6f8b2c969f28a930aaa5fd8f851a465202b480c4
Fred Zhang <zhangxu.fred@gmail.com>
2018-09-25 20:10:04 +0800

    make big index

---
+++ make_big_index.py
@@ -0,0 +1,20 @@
+def make_string(p):
+    s = ""
+    for e in p:
+        s = s + e
+    return s
+
+
+def make_big_index(size):
+    index = []
+    letters = ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a']
+    while len(index) < size:
+        word = make_string(letters)
+        add_to_index(index, word, 'fake')
+        for i in range(len(letters) - 1, 0, -1):
+            if letters[i] < 'z':
+                letters[i] = chr(ord(letters[i]) + 1)
+                break
+            else:
+                letters[i] = 'a'
+    return index
