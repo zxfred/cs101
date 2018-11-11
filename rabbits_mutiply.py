c844df90071920fca1c8f19bf0b53317d1b56941
Fred Zhang <zhangxu.fred@gmail.com>
2018-11-11 18:00:59 +0800

    rabbits_mutiply

---
+++ rabbits_mutiply.py
@@ -0,0 +1,61 @@
+# Rabbits Multiplying
+
+# A (slightly) more realistic model of rabbit multiplication than the Fibonacci
+# model, would assume that rabbits eventually die. For this question, some
+# rabbits die from month 6 onwards.
+#
+# Thus, we can model the number of rabbits as:
+#
+# rabbits(1) = 1 # There is one pair of immature rabbits in Month 1
+# rabbits(2) = 1 # There is one pair of mature rabbits in Month 2
+#
+# For months 3-5:
+# Same as Fibonacci model, no rabbits dying yet
+# rabbits(n) = rabbits(n - 1) + rabbits(n - 2)
+#
+#
+# For months > 5:
+# All the rabbits that are over 5 months old die along with a few others
+# so that the number that die is equal to the number alive 5 months ago.
+# Before dying, the bunnies reproduce.
+# rabbits(n) = rabbits(n - 1) + rabbits(n - 2) - rabbits(n - 5)
+#
+# This produces the rabbit sequence: 1, 1, 2, 3, 5, 7, 11, 16, 24, 35, 52, ...
+#
+# Define a procedure rabbits that takes as input a number n, and returns a
+# number that is the value of the nth number in the rabbit sequence.
+# For example, rabbits(10) -> 35. (It is okay if your procedure takes too
+#                                long to run on inputs above 30.)
+
+
+def rabbits(n):
+    if n == 1 or n == 2:
+        return 1
+    if 3 <= n <= 5:
+        return rabbits(n - 1) + rabbits(n - 2)
+    if n > 5:
+        return rabbits(n - 1) + rabbits(n - 2) - rabbits(n - 5)
+
+
+def iter_rabbits(n):
+    current = 0
+    after1 = 1
+    after2 = 1
+    after3 = 2
+    after4 = 3
+    after5 = 5
+    for i in range(0, n):
+        current, after1, after2, after3, after4, after5 = after1, after2, after3, after4, after5, after4 + after5 - after1
+    return current
+
+
+# print rabbits(30)
+# >>> 35
+# print iter_rabbits(30)
+
+s = ""
+for i in range(1, 12):
+    s = s + str(iter_rabbits(i)) + " "
+print s
+# >>> 1 1 2 3 5 7 11 16 24 35 52
+
