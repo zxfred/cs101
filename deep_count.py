5bee96a92778818b1069d9fe9b128220e860fc85
Fred Zhang <zhangxu.fred@gmail.com>
2018-11-16 20:27:46 +0800

    deep_count

--- deep_count.py
+++ deep_count.py
@@ -20,17 +20,23 @@ def is_list(p):
 # input is a List, and returns False otherwise.
 
 def deep_count(p):
+    sum = 0
+    for element in p:
+        sum = sum + 1
+        if is_list(element):
+            sum = sum + deep_count(element)
+    return sum
 
-# print deep_count([1, 2, 3])
+
+print deep_count([1, 2, 3])
 # >>> 3
 
 # The empty list still counts as an element of the outer list
-# print deep_count([1, [], 3])
+print deep_count([1, [], 3])
 # >>> 3
 
-# print deep_count([1, [1, 2, [3, 4]]])
+print deep_count([1, [1, 2, [3, 4]]])
 # >>> 7
 
-# print deep_count([[[[[[[[1, 2, 3]]]]]]]])
+print deep_count([[[[[[[[1, 2, 3]]]]]]]])
 # >>> 10
-
